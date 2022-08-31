06322b11818d874c7e92bbd36588e44fc124d278
Noah Lundquist <nalundquist@gmail.com>
2022-08-31 15:27:48 -0700

    WIP: push to analyze emergent bug in gaussDisp
    Co-authored by:  Samuel Majerus <ladolego@gmail.com>

--- src/daycalc.js
+++ src/daycalc.js
@@ -2,6 +2,7 @@ export const gaussDisparate = function(d, m, Y){ //'d': day of month (1 to 31). 
 	let c = parseInt(Y / 100);
 	let y = Y - (100*c).toFixed(); //last two digits of Y
 	let w = parseInt(((d + (2.6*m -0.2) + y + (y/4) + (c/4) - (2*c)) % 7).toFixed());  //0=Sunday, 1=Monday, ..., 6=Saturday. 
+	console.log("w = " + w);
 	let resultArray = [c, y, w];
 	let wkdayInd = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
 	resultArray[2] = wkdayInd[w]; 
