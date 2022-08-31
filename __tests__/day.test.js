462f0729383ea3cc236c8dc1a916225a70f91812
Noah Lundquist <nalundquist@gmail.com>
2022-08-31 14:51:54 -0700

    added second and third tests
    Co-authored by:  Samuel Majerus <ladolego@gmail.com>

--- __tests__/day.test.js
+++ __tests__/day.test.js
@@ -3,8 +3,19 @@ import { gaussDisparate } from "./../src/daycalc.js";
 describe('gaussDisparate', () => {
 
 	test('holds "c" (century) variable that is Y divided by 100 (parsed to an integer)', () => {
-		const c = gaussDisparate(10, 4, 1999);
+		const c = gaussDisparate(10, 4, 1999)[0];
 		console.log(c);
 		expect(c).toEqual(19);
 	});
+
+	test('holds "y" which is Y minus the result of "c" multiplied by 100', () => {
+		const y = gaussDisparate(10, 4, 1999)[1];
+		console.log(y);
+		expect(y).toEqual(99);
+	});
+	
+	test('returns an integer between 0-6 that relates to a day of the week incrementing by one starting at 0 = "Sunday"', () => {
+		const w = gaussDisparate(10, 4, 1999)[2];
+		expect(w).toEqual(6);
+	}); 
 });
--- src/daycalc.js
+++ src/daycalc.js
@@ -1,5 +1,11 @@
-export const gaussDisparate = function(d, m, Y){ //'d': day of month (1 to 31).  'm': shifted month (1=March, 2=April, ..., February=12).  'Y': 4-digit year. 
-	const c = parseInt(Y / 100);
-	return c;
-}
+export const gaussDisparate = function(d, m, Y){ //'d': day of month (1 to 31).  'm': month (1=Jan, 2=Feb, ..., 12=December).  'Y': 4-digit year. 
+	let c = parseInt(Y / 100);
+	let y = Y - (100*c).toFixed(); //last two digits of Y
+	let w = parseInt(((d + (2.6*m -0.2) + y + (y/4) + (c/4) - (2*c)) % 7).toFixed());  //0=Sunday, 1=Monday, ..., 6=Saturday. 
+	let resultArray = [c, y, w];
+	
+	let wkdayInd = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+	return resultArray; 
+
+};
 
