bc08c051442f7f0806bac535f96d7e48a6a49814
Samuel Majerus <ladolego@gmail.com>
2022-09-01 15:33:41 -0700

    Added an if-else case to UI Logic that tells user that their date is invalid if--and only if-- they try clicking the 'Go' button without entering anything. Also added CSS styling, including a FadeIn property (credit to Noah for that one).  [WIP: polishing things up]

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 /*     User Interface (UI) Logic     */
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './css/styles.css';
+import './../css/styles.css';
 import { gaussAlg } from './daycalc.js'; 
 
 function handleForm(event) {
@@ -13,18 +13,24 @@ function handleForm(event) {
   // const month = parseInt(document.querySelector('#length1').value);
   // const day = parseInt(document.querySelector('#length2').value);
   // const year = parseInt(document.querySelector('#length3').value);
-  const dateInput = document.querySelector("input#date-input").value;
-  console.log("inputted date is at first formatted and read as such: " + dateInput);
-  const date = new Date(dateInput); //NTS: double-check syntax 
-  console.log('date variable is read as "typeof:"' + typeof date);
-  console.log("Compared to 'dateInput', the Date object itself has this format: " + date);
+  const dateInput = document.querySelector("input#date-input").value; 
+  if(dateInput === ""){
+    const pTag = document.createElement("p");
+    divResult.classList.remove("hidden");
+    divResult.append(pTag);
+    pTag.append("Please input a valid date");
+  } else {
+    console.log("inputted date is at first formatted and read as such: " + dateInput);
+    const date = new Date(dateInput); //NTS: double-check syntax 
+    console.log('date variable is read as "typeof:"' + typeof date);
+    console.log("Compared to 'dateInput', the Date object itself has this format: " + date);
 
-  const response = gaussAlg(date.getUTCMonth(), date.getUTCDate(), date.getUTCFullYear())[1]; 
-  const pTag = document.createElement("p");
-  divResult.classList.remove("hidden");
-  divResult.append(pTag);
-  pTag.append(`Your result is: ${response}.`);
-  document.querySelector('div#weekday-output').append(pTag);
+    const response = gaussAlg(date.getUTCMonth(), date.getUTCDate(), date.getUTCFullYear())[1]; 
+    const pTag = document.createElement("p");
+    divResult.classList.remove("hidden");
+    divResult.append(pTag);
+    pTag.append(`Your result is: ${response}.`);
+  }
 }
 
 window.addEventListener("load", function() { 
