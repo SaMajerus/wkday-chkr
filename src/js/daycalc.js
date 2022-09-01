44ddec09be622029fa3ce1535b06a3b5dd78f444
Samuel Majerus <ladolego@gmail.com>
2022-09-01 15:59:16 -0700

    Commented out all console log statements

--- src/js/daycalc.js
+++ src/js/daycalc.js
@@ -1,8 +1,8 @@
 /*     Business Logic     */
-export const gaussAlg = function(M, d, Y){  //'d': day of month (1 to 31).  'M': month (1=Jan, 2=Feb, ..., 12=Dec).  'Y': 4-digit year #. 
-	console.log("gaussAlg function-- 'M' parameter:  " + M); 
-	console.log("gaussAlg function-- 'd' parameter:  " + d);
-	console.log("gaussAlg function-- 'Y' parameter:  " + Y);
+export function gaussAlg(M, d, Y){  //'d': day of month (1 to 31).  'M': month (1=Jan, 2=Feb, ..., 12=Dec).  'Y': 4-digit year #. 
+	// console.log("gaussAlg function-- 'M' parameter:  " + M); 
+	// console.log("gaussAlg function-- 'd' parameter:  " + d);
+	// console.log("gaussAlg function-- 'Y' parameter:  " + Y);
 
 	let m; //Stores month-offset value based on given 'M' and 'Y' values. 
 	let monthOffsetCommonYrs = [0,3,3,6,1,4,6,2,5,0,3,5];  //monthOffsetCommonYrs[0] = Jan, [1]=Feb, ...
@@ -12,9 +12,9 @@ export const gaussAlg = function(M, d, Y){  //'d': day of month (1 to 31).  'M':
 		m = monthOffsetLeapYrs[M]; 
 	} else { 
 		m = monthOffsetCommonYrs[M]; 
-	}; 
+	} 
 	const w = ((d%7) + (m%7) + ((5*((Y-1)%4))%7)  +  ((4*((Y-1)%100))%7)  +  ((6*((Y-1)%400))%7))%7;  //0=Sunday, 1=Monday, ..., 6=Saturday.
-	console.log("w = " + w); 
+	// console.log("w = " + w); 
 
 	let resultArray = [m, w];
 	let wkdayInd = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
@@ -24,7 +24,7 @@ export const gaussAlg = function(M, d, Y){  //'d': day of month (1 to 31).  'M':
 	
 	return resultArray; 
 	//return resultArray[1];
-};
+}
 
 // 'A'=year number, 'M'= month, 'D' = day.  'm' = month-offset (see 'table of month offsets'). 
 // w = R(D + m +  5*R((A-1)%4)  +  4R((A-1)%100)  +  6R((A-1)%400)) % 7
--- src/js/index.js
+++ src/js/index.js
@@ -10,9 +10,6 @@ function handleForm(event) {
   divResult.innerText = null;
   divResult.classList.add("hidden");
   
-  // const month = parseInt(document.querySelector('#length1').value);
-  // const day = parseInt(document.querySelector('#length2').value);
-  // const year = parseInt(document.querySelector('#length3').value);
   const dateInput = document.querySelector("input#date-input").value; 
   if(dateInput === ""){
     const pTag = document.createElement("p");
@@ -20,10 +17,10 @@ function handleForm(event) {
     divResult.append(pTag);
     pTag.append("Please input a valid date");
   } else {
-    console.log("inputted date is at first formatted and read as such: " + dateInput);
+    //console.log("inputted date is at first formatted and read as such: " + dateInput);
     const date = new Date(dateInput); //NTS: double-check syntax 
-    console.log('date variable is read as "typeof:"' + typeof date);
-    console.log("Compared to 'dateInput', the Date object itself has this format: " + date);
+    //console.log('date variable is read as "typeof:"' + typeof date);
+    //console.log("Compared to 'dateInput', the Date object itself has this format: " + date);
 
     const response = gaussAlg(date.getUTCMonth(), date.getUTCDate(), date.getUTCFullYear())[1]; 
     const pTag = document.createElement("p");
